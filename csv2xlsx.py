39a0d3c0182d60cc2bd3837ce56aeab74cf0fb68
Mike Guilmot <mguilmot@gmail.com>
2017-04-06 10:25:37 +0200

    initial file

---
+++ csv2xlsx.py
@@ -0,0 +1,44 @@
+'''
+Generates an Excel (2010) file from CSV
+Uses the openpyxl module : https://openpyxl.readthedocs.io/en/default/
+    Install it via 'pip install openpyxl'
+'''
+
+### Modules
+import csv, openpyxl
+
+### Variables
+f_in = "c:\\temp\\in.csv"           # input file
+f_out = "c:\\temp\\out.xlsx"        # output file
+delimiter=","                       # delititer the csv file uses. Standard = ",". Use "\t" for tab
+
+### Function(s)
+def writeXl(f_in=f_in,f_out=f_out,delimiter=delimiter):
+    '''
+        Converts our CSV file to XLSX format
+    '''
+    print("Starting conversion from CSV to xlsx")
+    print()
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    with open(f_in) as f:
+        reader = csv.reader(f, delimiter=delimiter)
+        for row in reader:
+            ws.append(row)
+    
+        dims = {}
+        for row in ws.rows:
+            for cell in row:
+                if cell.value:
+                    dims[cell.column] = max((dims.get(cell.column, 0), len(cell.value)))
+        for col, value in dims.items():
+            ws.column_dimensions[col].width = value
+    
+    wb.save(f_out)
+    print("Done")
+    print()
+
+### Example
+# writeXl(f_in="c:\\temp\\meh.csv",f_out="c:\\temp\\meh.xlsx",delimiter="\t")
+
