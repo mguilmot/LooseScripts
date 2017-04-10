16b493a36d56dc0af5cb66fe8c0232fc66544453
Mike Guilmot <mguilmot@gmail.com>
2017-04-10 07:55:29 +0200

    Removed using files, added using dicts and lists

--- AD_Build_ACL_Suffixes.py
+++ AD_Build_ACL_Suffixes.py
@@ -1,7 +1,6 @@
 '''
     Building access list based on AD groups
-    First version, working with log files as output, then reading them to build the ACL
-    Will adapt later with lists and dicts
+    Second version, simplified, working with dictionaries, then build the ACL
     Same as the other script, but handles if you have AD groups with different suffixes for RO and RW groups
     ex: 
         ADgroupName-Usr = RO
@@ -11,18 +10,24 @@
     
     Uses:
     - openpyxl (pip install openpyxl)
+    - psutil (pip install psutil)
     - pyad (pip install https://github.com/zakird/pyad/archive/master.zip)
+    
+    TODO:
+    - search for AD groups (instead of input txt file)
+    - simplify even further (2 dictionaries instead of 3)
 '''
 
-import os, csv, time, random, shutil
+import os, csv, time, psutil
 import openpyxl
 from openpyxl.styles import Font, Alignment, Border, Side
 from pyad import *
 
 ### Vars
-fileName = "c:\\temp\in.txt"                            # Filename containing the AD groups we should check
+fileName_in = "c:\\temp\in.txt"                         # Filename containing the AD groups we should check
 fileName_csv = "c:\\temp\out.csv"                       # Output CSV filename
 fileName_xl = "c:\\temp\\out.xlsx"                      # Output Excel filename
+generate_xl = True                                      # Do you want to generate the Excel file
 start_time = time.time()                                # Start time
 suffixignore="-share"                                   # Suffix of AD groups we should ignore
 suffixRO="-usr"                                         # Suffix for AD groups giving read only access
@@ -31,59 +36,22 @@ suffixLen=len(suffixRO)                                 # Length of the suffixes
 header_row = ['Access List','User ID']                  # Header row1 for our CSV file
 header_row2 = ["Group description","AD group prefix"]   # Header row2 for our CSV file
 delimiter = ","                                         # Delimiter for our CSV file
-lstUsers=[]                                             # Placeholder
+dictAllGroups = {}                                      # Placeholder
 dictAllUsers = {}                                       # Placeholder
-
-def genFolderName(num=18):
-    '''
-        Generates a foldername to store our temporary log files in.
-        Folder will be deleted after use.
-    '''
-    chars = "abcdefghijklmnopqrstuvwxyz0123456789"
-    folderName = "_"
-    for i in range(num):
-        ran = random.randint(0,len(chars)-1)
-        folderName += chars[ran]
-    return folderName
-
-folderName = "c:\\temp\\" + str(genFolderName())
-
-# Create folder if it does not exist yet
-if not os.path.isdir(folderName):
-    os.makedirs(folderName)
-
-def countLenItems(lst):
-    t=0
-    for c in lst:
-        t += len(c)
-    return t
-    
-def countLines(fileName=fileName):
-    '''
-        Counting the lines in our log files
-    '''
-    c = 0
-    with open(fileName) as f:
-        for l in f:
-            line = l.strip("\n")
-            if line.startswith("#") or len(line)<2:
-                continue
-            else:
-                c += 1
-    return c    
+dictAllUsersInfo = {}                                   # Placeholder
     
 def printmsg(text="",lines=False):
     '''
         Simple function to print the text we want as output.
     '''
     c = len(text)
-    l = c*"-"
     if lines == True:
+        l = c*"-"
         return text + "\n" + l + "\n"
     else:
         return "\n" + text + "\n"
 
-def readFile(fileName=fileName,suffixignore=suffixignore):
+def readFile(fileName=fileName_in,suffixignore=suffixignore):
     '''
         Reads the input file, returns them 1 by 1
         Empty lines and lines starting with # are ignored
@@ -97,163 +65,111 @@ def readFile(fileName=fileName,suffixignore=suffixignore):
             else:
                 yield line
 
-def retLogFiles():
-    '''
-        Returns the logfiles in our folderName\*
-    '''
-    for part in os.walk(folderName+"\."):
-        for file in part:
-            if len(file) <2:
-                continue
-            elif isinstance(file,list):
-                for filepart in file:
-                    if len(filepart)<2 or os.stat(folderName+"\\"+filepart).st_size == 0:
-                        continue
-                    else:
-                        yield filepart
-            else:
-                if os.path.isdir(file):
-                    continue
-                else:
-                    yield file
-
-def retUserName(user="Administrator"):
+def retDispName(user="Administrator"):
     try:
         userObj = pyad.aduser.ADUser.from_cn(user)
         return userObj.get_attribute("displayName")[0]
     except:
-        return ""
+        return "None"
 
-def writeCsv(fileName_csv=fileName_csv,delimiter=delimiter,header_row=header_row,header_row2=header_row2,folderName=folderName,dictAllUsers=dictAllUsers,suffixLen=suffixLen,suffixRO=suffixRO,suffixRW=suffixRW):
+def writeCsv(fileName_csv=fileName_csv,delimiter=delimiter,header_row=header_row,header_row2=header_row2,dictAllUsers=dictAllUsers,suffixLen=suffixLen,suffixRO=suffixRO,suffixRW=suffixRW):
     '''
         Writes the desired information to CSV file.
     '''
     
-    # dictAccess will be {"filename without suffix without .txt":["description",[RO access list],[RW access list]]}
-    dictAccess = {}
-    
-    print(printmsg(text="Reading logfiles to generate CSV"))
+    # dictAllGroupsInfo will be {"filename without suffix without .txt":["description",[RO access list],[RW access list]]}
+    dictAllGroupsInfo = {}
 
     with open(fileName_csv,"w") as f:
         writer = csv.writer(f,delimiter=delimiter,dialect="excel",lineterminator="\n")
         
         print(printmsg(text="Generating CSV file."))
         
-        # Print header row
+        # Print header rows
         writer.writerow(header_row)
-        print(printmsg(text="Getting display names."))
-        for user in header_row:
-            displayName = retUserName(user)
-            if displayName != "":
-                header_row2.append(displayName)
         writer.writerow(header_row2)
         
-        # Reading logfiles 1 by 1, creating dictAccess
-        for fileNum,fn in enumerate(retLogFiles()):
-            file = folderName + "\\" + fn
-            
-            toStrip = suffixLen + 4
-            folderNameLen = len(folderName)+1
-            
-            fileName_stripped = file[folderNameLen:-toStrip]
-
-            if file[folderNameLen:-toStrip] in dictAccess:
-                # file already checked once, we don't have to check the desc anymore
-                
-                if file[-toStrip:] == suffixRW+".txt":
-                    users = []
-                    for num,line in enumerate(readFile(file)):
-                        if num > 0:
-                            users.append(line)
-                    dictAccess[file[folderNameLen:-toStrip]][2] = users
+        # Going over dictAllGroups, creating dictAccess to combine RO + RW groups
+        for groupName in dictAllGroups:
+            groupNameShort = groupName[:-suffixLen]
+            groupNameSuffix = groupName[-suffixLen:]
+        
+            if groupNameShort in dictAllGroupsInfo:
+                # Group pair was already checked once, we need to add the other part (RO/RW)
+                if groupNameSuffix.lower() == suffixRO.lower():
+                    dictAllGroupsInfo[groupNameShort][1] = dictAllGroups[groupName][1]
                 else:
-                    users = []
-                    for num,line in enumerate(readFile(file)):
-                        if num > 0:
-                            users.append(line)
-                    dictAccess[file[folderNameLen:-toStrip]][1] = users
+                    dictAllGroupsInfo[groupNameShort][2] = dictAllGroups[groupName][1]
+                # Finding and removing users that are in both RO and RW groups
+                setCommon = set.intersection(set(dictAllGroupsInfo[groupNameShort][1]),set(dictAllGroupsInfo[groupNameShort][2]))
+                for doubleUser in setCommon:
+                    dictAllGroupsInfo[groupNameShort][1].remove(doubleUser)
             else:
-                if file[-toStrip:] == suffixRW+".txt":
-                    users = []
-                    for num,line in enumerate(readFile(file)):
-                        if num == 0:
-                            description = line
-                        else:
-                            users.append(line)
-                        dictAccess[file[folderNameLen:-toStrip]] = [description,[],users]
+                dictAllGroupsInfo[groupNameShort] = [dictAllGroups[groupName][0],[],[]]
+                if groupNameSuffix.lower() == suffixRO.lower():
+                    dictAllGroupsInfo[groupNameShort][1] = dictAllGroups[groupName][1]
                 else:
-                    users = []
-                    for num,line in enumerate(readFile(file)):
-                        if num == 0:
-                            description = line
-                        else:
-                            users.append(line)
-                        dictAccess[file[folderNameLen:-toStrip]] = [description,users,[]]
-            
-            
-            
-            
-        # Delete users in RO lists that also have RW
-        lstKeys = list(dictAccess.keys())
-        lstKeys.sort()
-        for group in lstKeys:
-            for ROuser in dictAccess[group][1]:
-                if ROuser in dictAccess[group][2]:
-                    dictAccess[group][1].remove(ROuser)
-        
-               
-        
-        for item in lstKeys:
+                    dictAllGroupsInfo[groupNameShort][2] = dictAllGroups[groupName][1]
+                    
+        # Writing the actual lines in the CSV
+        for item in sorted(list(dictAllGroupsInfo.keys())):
         
             linetxt = [""] * 2
             usersinfile = [""] * len(dictAllUsers) 
             
-            linetxt[0] = dictAccess[item][0]
-            linetxt[1] = item
+            linetxt[0] = dictAllGroupsInfo[item][0][0]  # group desc
+            linetxt[1] = item                           # group name without suffix
             
-            for userRO in dictAccess[item][1]:
-                if not userRO.startswith("="):
-                    userlocation = dictAllUsers[userRO.upper()]
-                    usersinfile[userlocation]="RO"
-            for userRW in dictAccess[item][2]:
-                if not userRW.startswith("="):
-                    userlocation = dictAllUsers[userRW.upper()]
-                    usersinfile[userlocation]="RW"
+            for userRO in dictAllGroupsInfo[item][1]:
+                userlocation = dictAllUsers[userRO.upper()]
+                usersinfile[userlocation]="RO"
+            for userRW in dictAllGroupsInfo[item][2]:
+                userlocation = dictAllUsers[userRW.upper()]
+                usersinfile[userlocation]="RW"
+            
             writer.writerow(linetxt+usersinfile)
         
-        writer.writerow([])
-        writer.writerow([])
-        writer.writerow(["RO = Read Only access"])
-        writer.writerow(["RW = Read + Write access"])
-        
+        writer.writerow([])                             # empty line
+        writer.writerow([])                             # empty line
+        writer.writerow(["RO = Read Only access"])      # extra info
+        writer.writerow(["RW = Read + Write access"])   # extra info
         
         print(printmsg(text="Done. Check " + fileName_csv + " for output."))
 
-def writeLog(groupName,lstUsers=lstUsers):
+def findUsers(groupName="Administrators",dictAllGroups=dictAllGroups,dictAllUsers=dictAllUsers,dictAllUsersInfo=dictAllUsersInfo):
     '''
-        Writes 1 file per AD group as text file.
-        Writes:
-        - description
-        - members (1 per line)
-        If no members: file = empty
+        Finds users that are members of the AD group, adds them to dictAllUsers if not already in there
+        Adds the group and group description and all members to dictAllGroups
     '''
-    grpObj = pyad.adgroup.ADGroup.from_cn(groupName)
-    grpMembers = pyad.adgroup.ADGroup.get_members(grpObj)
-    members = [str(member).split(",")[0][12:] for member in grpMembers]
-    with open(folderName + "\\" + groupName + ".txt","w") as f:
-        if len(members)>0:
+    lstUsers = []
+    try:
+        grpObj = pyad.adgroup.ADGroup.from_cn(groupName)
+        grpMembers = pyad.adgroup.ADGroup.get_members(grpObj)
+        users = [str(member).split(",")[0][12:] for member in grpMembers]
+        if len(users)>0:
             desc = grpObj.get_attribute("description")[0]
-            if len(desc)<2:
-                desc="No description"
-            f.write(desc)
-            f.write("\n")
-        for member in members:
-            if member not in lstUsers:
-                lstUsers.append(member)
-            f.write(member)
-            f.write("\n")
+            if desc == "":
+                desc="No group description"
+            for user in users:
+                if user.startswith("="):
+                    # user = AD group
+                    continue
+                else:
+                    # Group is not empty, user is not an AD group
+                    lstUsers.append(user.upper())
+                    if user not in dictAllUsers:
+                        displayName = retDispName(user=user)
+                        dictAllUsersInfo[user] = displayName
+            dictAllGroups[groupName] = [[desc],lstUsers]
+            return len(lstUsers)
+        else:
+            # Group has no members
+            return "0. Skipping."
+    except:
+        # Group does not exist
+        return "0. Skipping."
 
-def writeXl(fileName_csv=fileName_csv,fileName_xl=fileName_xl,delimiter=delimiter,folderName=folderName):
+def writeXl(fileName_csv=fileName_csv,fileName_xl=fileName_xl,delimiter=delimiter):
     '''
         Converts our CSV file to XLSX format
     '''
@@ -310,35 +226,27 @@ def writeXl(fileName_csv=fileName_csv,fileName_xl=fileName_xl,delimiter=delimite
 
     print(printmsg(text="Done. Check " + fileName_xl + " for output."))
     
-# Start of our script       
-print(printmsg(text="Writing log files with members.",lines=True))
-
-# Writing our temporary log files to folderName
+# Reading input file: filling dictionaries with requested data
 for line in readFile():
-    writeLog(line)
-    print("Writing",line)
-print(printmsg(text="Done."))
+    print("Looking for users for:",line,end="")
+    print(". Found:",findUsers(groupName=line))
 
-# Create dictionary with theire number alphabetically, and add them to our header
-allUsers = sorted(lstUsers)
-
-# Since we will not add everything we'll need a puppy integer following our list
-puppy = 0
-for user in allUsers:
-    if not user.startswith("="):
-        dictAllUsers[user]=puppy
+# Filling header rows with our users in alphabetical order. Filling dict to remember their location for our CSV
+for num,user in enumerate(sorted(dictAllUsersInfo)):
+        dictAllUsers[user]=num
         header_row.append(user)
-        puppy += 1
+        header_row2.append(dictAllUsersInfo[user])
 
 # Writing our output files
-writeCsv()
-writeXl()
-
-# Wrapping up
-shutil.rmtree(folderName, ignore_errors=True)
-print(printmsg(text="Deleting our temporary log files.\nDone."))
+if len(dictAllUsers)>0:
+    writeCsv()
+    if generate_xl == True:
+        writeXl()
+else:
+    print("No data. Skipping CSV and XLS.\n")
 
 # Print execution time
 print("Execution time: --- %s seconds ---" % '{:02.2f}'.format((time.time() - start_time)))
+process = psutil.Process(os.getpid())
+print("Memory Usage: --- %s MB ---" % '{:02.2f}'.format((process.memory_info().rss / float(2 ** 20))))
 print()
-
